27e460b340a7866576e263173f868c90fd069fa7
hasibul hasan <hasibul.hasan@sslwireless.com>
2021-09-01 11:10:09 +0600

    api connection check

---
+++ src/scripts/api.js
@@ -0,0 +1,29 @@
+// USER 
+export const LOGIN = "user/login";
+export const CHANGE_PASSWORD = "user/password/change";
+export const USER_PROFILE = "user/profile";
+export const EMAIL_CONFIRMATION_RESENT = "user/email/resend";
+export const EMAIL_CONFIRMATION_VERIFY = "user/email/verify/";
+export const USER_LIST = "user/list";
+export const USER_CREATE = "user/create";
+export const USER_UPDATE = "user/update";
+
+// MERCHENT 
+export const MERCHENT_LIST = "merchant/list";
+export const MERCHENT_SELT_SIGNUP = "merchant/self-signup";
+export const MERCHENT_CREATE = "merchant/create";
+export const MERCHENT_CREATE = "merchant/update";
+
+// PERMISSION 
+export const PERMISSION_LIST = "permission/list";
+export const PERMISSION_CREATE = "permission/create";
+export const PERMISSION_DELETE = "permission/delete";
+export const PERMISSION_UPDATE = "permission/update";
+
+//ROLE
+export const ROLE_LIST = "role/list";
+export const ROLE_CREATE = "role/create";
+export const ROLE_DELETE = "role/delete";
+export const ROLE_UPDATE = "role/update";
+
+
