64f93792295c4d63123a344bda8c1b4af22100ad
hasibul hasan <hasibul.hasan@sslwireless.com>
2021-09-16 23:23:23 +0600

    update

--- src/pages/private/Dashboard/Doughnut.js
+++ src/pages/private/Dashboard/Doughnut.js
@@ -1,40 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# Done',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
 
-const DoughnutChart = () => (
-  <>
-    <div className='header'>
-      <h1 className='title'>Product Model</h1>
-    </div>
-    <Doughnut data={data} />
-  </>
-);
+const DoughnutChart = ({products}) => {
+  const [chart, setChart] = useState()
+
+  useEffect(() => {
+    let labels = products.map(pro => pro.product.product_name),
+        quantity = products.map(pro => pro.quantity);
+
+    setChart({
+      labels: labels,
+      datasets: [
+        {
+          label: '# Done',
+          data: quantity,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    })
+
+  }, [products])
+
+  // const data = {
+  //   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //   datasets: [
+  //     {
+  //       label: '# Done',
+  //       data: [12, 19, 3, 5, 2, 3],
+  //       backgroundColor: [
+  //         'rgba(255, 99, 132, 0.2)',
+  //         'rgba(54, 162, 235, 0.2)',
+  //         'rgba(255, 206, 86, 0.2)',
+  //         'rgba(75, 192, 192, 0.2)',
+  //         'rgba(153, 102, 255, 0.2)',
+  //         'rgba(255, 159, 64, 0.2)',
+  //       ],
+  //       borderColor: [
+  //         'rgba(255, 99, 132, 1)',
+  //         'rgba(54, 162, 235, 1)',
+  //         'rgba(255, 206, 86, 1)',
+  //         'rgba(75, 192, 192, 1)',
+  //         'rgba(153, 102, 255, 1)',
+  //         'rgba(255, 159, 64, 1)',
+  //       ],
+  //       borderWidth: 1,
+  //     },
+  //   ],
+  // };
+
+  return (
+    <>
+      <div className='header'>
+        <h1 className='title'>Product Model</h1>
+      </div>
+      <Doughnut data={chart} />
+    </>
+  )
+};
 
 export default DoughnutChart;
--- src/pages/private/Dashboard/index.js
+++ src/pages/private/Dashboard/index.js
@@ -2,7 +2,7 @@ import React, { Fragment, useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import VerticalBar from "./verticalBar";
 import DoughnutChart from "./Doughnut";
-import { Card } from 'antd';
+import { Card, Skeleton  } from 'antd';
 import { Trans, useTranslation } from 'react-i18next';
 import { authContext } from "../../../context/AuthContext";
 import { postData } from '../../../scripts/api-service';
@@ -29,72 +29,77 @@ export default function Dashboard() {
     return (
         <Fragment>
             {
-                user && <div className="rui-page-title">
+                dashboard ? <>
+                {
+                    user && <div className="rui-page-title">
+                        <div className="container-fluid">
+                            <nav aria-label="breadcrumb">
+                                <ol className="breadcrumb" style={{"list-style-type": "none"}}>
+                                    <li className="breadcrumb-item"> <strong>
+                                        {t("common.name")}: </strong> {user?.name}</li>
+                                </ol>
+                                <ol className="breadcrumb" style={{"list-style-type": "none"}}>
+                                    <li className="breadcrumb-item"> <strong>Company:</strong> {user.company_name} </li>
+                                </ol>
+                                <ol className="breadcrumb" style={{"list-style-type": "none"}}>
+                                    <li className="breadcrumb-item"> <strong>Phone:</strong> {user.phone} </li>
+                                </ol>
+                                <ol className="breadcrumb" style={{"list-style-type": "none"}}>
+                                    <li className="breadcrumb-item"> <strong>Email:</strong> {user.email} </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                }
+
+                <div className="rui-page-content">
                     <div className="container-fluid">
-                        <nav aria-label="breadcrumb">
-                            <ol className="breadcrumb" style={{"list-style-type": "none"}}>
-                                <li className="breadcrumb-item"> <strong>
-                                    {t("common.name")}: </strong> {user?.name}</li>
-                            </ol>
-                            <ol className="breadcrumb" style={{"list-style-type": "none"}}>
-                                <li className="breadcrumb-item"> <strong>Company:</strong> {user.company_name} </li>
-                            </ol>
-                            <ol className="breadcrumb" style={{"list-style-type": "none"}}>
-                                <li className="breadcrumb-item"> <strong>Phone:</strong> {user.phone} </li>
-                            </ol>
-                            <ol className="breadcrumb" style={{"list-style-type": "none"}}>
-                                <li className="breadcrumb-item"> <strong>Email:</strong> {user.email} </li>
-                            </ol>
-                        </nav>
+                        {
+                            dashboard && <div className="row xs-gap mt-20 px-20">
+                                <div className="col  col-sm-12 col-lg-3 mb-10">
+                                    <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
+                                        <h1>Product Stock</h1>
+                                        <Link to="/product-stock"><h2>{dashboard.product_stock}</h2></Link>
+                                    </div>
+                                </div>
+                                <div className="col  col-sm-12 col-lg-3 mb-10">
+                                    <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
+                                        <h1>Product Order</h1>
+                                        <Link to="/product-order"><h2>{dashboard.product_order}</h2></Link>
+                                    </div>
+                                </div>
+                                <div className="col col-sm-12 col-lg-3 mb-10">
+                                    <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
+                                        <h1>Low Stock</h1>
+                                        <Link to="/low-stock"><h2>{dashboard.low_stock}</h2></Link>
+                                    </div>
+                                </div>
+                                <div className="col  col-sm-12 col-lg-3 mb-10">
+                                    <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
+                                        <h1>Product Delivery</h1>
+                                        <Link to="/product-delivery"><h2>{dashboard.product_delivery}</h2></Link>
+                                    </div>
+                                </div>
+                            </div>
+                        }
+
+                        <div className="row xs-gap mt-20 px-20">
+                            <div className="col col-sm-12 col-lg-12 mb-10">
+                                <Card >
+                                    <VerticalBar quantity={dashboard.product_quantity_model_wise}></VerticalBar>
+                                </Card >
+                            </div>
+                            <div className="col col-sm-12 col-lg-12 mb-10">
+                                <Card >
+                                    <DoughnutChart products={dashboard.popular_product_model}></DoughnutChart>
+                                </Card>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                </> : Skeleton
             }
-
-            <div className="rui-page-content">
-                <div className="container-fluid">
-                    {
-                        dashboard && <div className="row xs-gap mt-20 px-20">
-                            <div className="col  col-sm-12 col-lg-3 mb-10">
-                                <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
-                                    <h1>Product Stock</h1>
-                                    <Link to="/product-stock"><h2>{dashboard.product_stock}</h2></Link>
-                                </div>
-                            </div>
-                            <div className="col  col-sm-12 col-lg-3 mb-10">
-                                <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
-                                    <h1>Product Order</h1>
-                                    <Link to="/product-order"><h2>{dashboard.product_order}</h2></Link>
-                                </div>
-                            </div>
-                            <div className="col col-sm-12 col-lg-3 mb-10">
-                                <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
-                                    <h1>Low Stock</h1>
-                                    <Link to="/low-stock"><h2>{dashboard.low_stock}</h2></Link>
-                                </div>
-                            </div>
-                            <div className="col  col-sm-12 col-lg-3 mb-10">
-                                <div className="bg-grey-1 pt-15 pr-20 pb-15 pl-20 br-4 text-center">
-                                    <h1>Product Delivery</h1>
-                                    <Link to="/product-delivery"><h2>{dashboard.product_delivery}</h2></Link>
-                                </div>
-                            </div>
-                        </div>
-                    }
-
-                    <div className="row xs-gap mt-20 px-20">
-                        <div className="col col-sm-12 col-lg-6 mb-10">
-                            <Card >
-                                <VerticalBar></VerticalBar>
-                            </Card >
-                        </div>
-                        <div className="col col-sm-12 col-lg-6 mb-10">
-                            <Card >
-                                <DoughnutChart></DoughnutChart>
-                            </Card>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </Fragment>
     )
 }
